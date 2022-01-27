f70f9be8205d4e47b5f7837874fe5af9a829bf32
jiangcunyou <jiangcunyou0112@hotmail.com>
2022-01-27 10:26:47 -0500

    20220128

--- src/Laicode/Algorithm/RecursionIII/FlattenBinaryTreetoLinkedList.java
+++ src/Laicode/Algorithm/RecursionIII/FlattenBinaryTreetoLinkedList.java
@@ -1,2 +1,48 @@
-package Laicode.Algorithm.RecursionIII;public class FlattenBinaryTreetoLinkedList {
+package Laicode.Algorithm.RecursionIII;
+
+/*
+* Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+* */
+
+public class FlattenBinaryTreetoLinkedList {
+    public TreeNode flatten(TreeNode root) {
+        // Write your solution here
+        TreeNode[] prev = new TreeNode[1];
+        helper(root, prev);
+
+        return root;
+    }
+
+    private void helper(TreeNode root, TreeNode[] prev){
+        if (root == null){
+            return;
+        }
+        helper(root.right, prev);
+        helper(root.left, prev);
+        root.right = prev[0];
+        root.left = null;
+        prev[0] = root;
+    }
 }
--- src/Laicode/Algorithm/RecursionIII/ReconstructBinaryTreeWithPreorderAndInorder.java
+++ src/Laicode/Algorithm/RecursionIII/ReconstructBinaryTreeWithPreorderAndInorder.java
@@ -1,2 +1,51 @@
-package Laicode.Algorithm.RecursionIII;public class ReconstructBinaryTreeWithPreorderAndInorder {
+package Laicode.Algorithm.RecursionIII;
+
+/*
+* Given the preorder and inorder traversal sequence of a binary tree, reconstruct the original tree.
+
+Assumptions
+
+The given sequences are not null and they have the same length
+There are no duplicate keys in the binary tree
+Examples
+
+preorder traversal = {5, 3, 1, 4, 8, 11}
+
+inorder traversal = {1, 3, 4, 5, 8, 11}
+
+the corresponding binary tree is
+
+        5
+
+      /    \
+
+    3        8
+
+  /   \        \
+
+1      4        11
+
+How is the binary tree represented?
+
+We use the pre order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+
+The sequence [1, 2, #, 3, 4, #, #, #] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+* */
+
+public class ReconstructBinaryTreeWithPreorderAndInorder {
+    public TreeNode reconstruct(int[] inOrder, int[] preOrder) {
+        // Write your solution here
+    }
 }
